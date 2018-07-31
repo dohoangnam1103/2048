dc7912d1faf9d299c509d336fe18d3586aff57ed
nam.d <nam.d@sutrixsolutions.com>
2018-07-31 11:37:00 +0700

    commit

---
+++ dist/precache-manifest.0e4535c9b21a1fa8c338d4d937dd4ff6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a33a6233c4cb7c12c8e4",
+    "url": "/js/chunk-vendors.a33a6233.js"
+  },
+  {
+    "revision": "dfdbbf3ed376d459c321",
+    "url": "/js/app.dfdbbf3e.js"
+  },
+  {
+    "revision": "a3912164dca9fcb3b11b",
+    "url": "/js/about.a3912164.js"
+  },
+  {
+    "revision": "8995ca33a8a3218239026fa041183b71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9bdac589c0d3aac828fd3a15108b61a",
+    "url": "/fonts/clear-sans.b9bdac58.ttf"
+  },
+  {
+    "revision": "a33a6233c4cb7c12c8e4",
+    "url": "/css/chunk-vendors.ca6be649.css"
+  },
+  {
+    "revision": "dfdbbf3ed376d459c321",
+    "url": "/css/app.d5b8e6c1.css"
+  }
+];
